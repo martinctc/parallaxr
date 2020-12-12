53dba646ee8d4fb10e6e34d5c53b347b6b870c81
Martin Chan <martinchan53@gmail.com>
2020-12-12 13:56:40 +0000

    feat: enable pretty CSS return (#6)
    The CSS code returned by default_css() does not now require additional formatting. There is also a new option to return a CSS file path by providing a path argument.

--- R/default_css.R
+++ R/default_css.R
@@ -1,12 +1,27 @@
 #' @title Return the default CSS document used in the parallaxr package
 #'
-#' @description This returns a character vector of length 1 containing the default
-#' CSS document used to generate the parallax scroll document.
+#' @description This returns a character vector of length 1 containing the
+#' default CSS document used to generate the parallax scroll document.
+#'
+#' @param path character. Path to where the CSS file should be saved, including
+#' a file name with the ".css" file extension.
+#' Defaults to NULL, where the css will be displayed on the console instead.
+#'
+#' @examples
+#' default_css()
 #'
 #' @export
-default_css <- function(){
+default_css <- function(path = NULL){
 
-  readLines(con = system.file("style.css", package = "parallaxr"), warn = FALSE) %>%
-    paste(collapse = " ")
+  return_css <-
+    readLines(con = system.file("style.css", package = "parallaxr"), warn = FALSE) %>%
+    paste(collapse = "\n")
 
+  if(is.null(path)){
+    message(return_css)
+
+  } else {
+
+    writeLines(return_css, con = path)
+  }
 }
